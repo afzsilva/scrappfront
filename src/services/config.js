a2c26ee044b01752ca3b003001715d9122b189f3
afzsilva <afzsilva@gmail.com>
2021-06-28 18:46:31 -0300

    Variavel de ambiente configurada

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-//http://127.0.0.1:8000/api/citacoes/
-
+const API_URL = process.env.VUE_APP_ROOT_API;
 export const http = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: API_URL,
 });
